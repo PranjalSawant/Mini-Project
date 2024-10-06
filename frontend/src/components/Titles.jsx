fc664c7910c262035457491e683338eafbd642b0
PranjalSawant <sawantpranjal22@gmail.com>
2024-10-06 18:03:16 +0530

    frontend new

--- frontend/src/components/Titles.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Titles = (props) => {
-  return (
-    <div>
-        <h2 className={`text-center fw-bold py-5 ${props.textColor}`}>{props.heading}</h2>
-    </div>
-  )
-}
